08ee445864920b5d859e6d86200cc9d9a1d12b22
Matt Crecraft <mcrecraft@gmail.com>
2022-06-01 10:17:24 -0500

    Upload

--- App.java
+++ App.java
@@ -1,6 +1,26 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+    String message = "Vehicle Information";
+    Integer vin = 1231234;
+    String make = "Ford";
+    String model = "F150";
+    String color = "Gray";
+    Boolean towingPackage = false;
+    Integer odometer = 456;
+    Integer price = 45000;
+    String qualityRating = "B";
+
+    StringBuilder vehicleInfo = new StringBuilder();
+    vehicleInfo.append(message);
+    vehicleInfo.append(": VIN-" + vin);
+    vehicleInfo.append("- MAKE/MODEL-" + make);
+    vehicleInfo.append(":" + model);
+    vehicleInfo.append(" COLOR-" + color);
+    vehicleInfo.append(" TOWING PKG- " + towingPackage);
+    vehicleInfo.append(" ODOMETER- " + odometer);
+    vehicleInfo.append(" PRICE- $" + price);
+    vehicleInfo.append(" QUALITY RATING- " + qualityRating);
+
+    System.out.println(vehicleInfo);
   }
 }
